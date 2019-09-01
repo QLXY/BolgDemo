a48b74cede9b5fa8e82c1e2114f210747236bf3b
QLXY <QLXY1018@outlook.com>
2019-09-01 23:26:02 +0800

    添加项目文件。

---
+++ CLanguageDLL/stdafx.cpp
@@ -0,0 +1,18 @@
+﻿#include "stdafx.h"
+
+extern "C" __declspec(dllexport) int __stdcall Add(int m, int n)
+{
+	return m + n;
+}
+C_int Subtract(int m, int n)
+{
+	return m - n;
+}
+C_int Multiply(int m, int n)
+{
+	return m * n;
+}
+C_int Divide(int m, int n)
+{
+	return m / n;
+}
---
+++ NETDLL/Libary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NETDLL
+{
+    public class Libary
+    {
+        [DllImport("CLanguageDLL.dll")]
+        public extern static int Add(int m, int n);
+        [DllImport("CLanguageDLL.dll")]
+        public extern static int Subtract(int m, int n);
+        [DllImport("CLanguageDLL.dll")]
+        public extern static int Multiply(int m, int n);
+        [DllImport("CLanguageDLL.dll")]
+        public extern static int Divide(int m, int n);
+    }
+}
